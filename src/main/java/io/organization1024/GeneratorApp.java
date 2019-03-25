4d1fae46c75350207f32e8962e96c6e24c20daa2
imyzt <imyzt01@gmail.com>
2019-03-25 16:21:50 +0800

    :tada: init commit

---
+++ src/main/java/io/organization1024/GeneratorApp.java
@@ -0,0 +1,16 @@
+package io.organization1024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author imyzt
+ */
+@SpringBootApplication
+public class GeneratorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorApp.class, args);
+	}
+
+}
